c77957712c66d7860c5507a39f90642c4b516929
hakatu <hungnguyen110697@gmail.com>
2021-05-31 01:52:46 +0700

    tried to push aes as a project
    tried to push aes as a project as the title says

---
+++ rtl/aes/tb_sha256_transform.v
@@ -0,0 +1,28 @@
+module tb_sha256_transform;
+
+////////clock generator//////////
+reg clk;
+
+always begin
+clk = 0;
+#1 clk=1;
+#1;
+end
+
+///////////////reg-wire//////////////
+
+reg feedback,
+reg [5:0] cnt,
+reg [255:0] rx_state,
+reg [511:0] rx_input,
+output reg [255:0] tx_hash
+////////////////////////////////
+
+sha256_transform isha256_transform(
+	.clk(clk),
+	.feedback(),
+	.cnt(),
+	.rx_state(),
+	.rx_input(),
+	.tx_hash()
+);
