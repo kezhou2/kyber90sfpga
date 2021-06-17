2794e4ea2374970f82528588daff3b1a34e60fde
hakatu <hungnguyen110697@gmail.com>
2021-06-17 16:30:32 +0700

    Update the library
    Update the library

--- rtl/aes/tb_sha256_transform.v
+++ rtl/aes/tb_sha256_transform.v
@@ -11,11 +11,12 @@ end
 
 ///////////////reg-wire//////////////
 
-reg feedback,
-reg [5:0] cnt,
-reg [255:0] rx_state,
-reg [511:0] rx_input,
-output reg [255:0] tx_hash
+reg feedback;
+reg [5:0] cnt;
+reg [255:0] rx_state;
+reg [511:0] rx_input;
+wire [255:0] tx_hash;
+
 ////////////////////////////////
 
 sha256_transform isha256_transform(
@@ -26,3 +27,7 @@ sha256_transform isha256_transform(
 	.rx_input(),
 	.tx_hash()
 );
+
+initial begin
+	cnt = 6'b0;
+end
--- rtl/macro/fflopx.v
+++ rtl/macro/fflopx.v
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////////
 //
-//  Arrive Technologies
+//  Hung's Technologies
 //
 // Filename        : fflopx.v
 // Description     : variable size flip flop
@@ -8,12 +8,13 @@
 // Author          : ducdd@atvn.com.vn
 // Created On      : Tue Jul 29 18:00:29 2003
 // History (Date, Changed By)
+// Thu Aug 09 11:21:02 2018, NGUYEN@WIN-1B3KG57AJQF change to sync ffx
 //
 //////////////////////////////////////////////////////////////////////////////////
 module fflopx
     (
      clk,
-     rst_,
+     rst,
      idat,
      odat
      );
@@ -21,16 +22,16 @@ module fflopx
 parameter WIDTH = 8;
 parameter RESET_VALUE = {WIDTH{1'b0}};
 
-input   clk, rst_;
+input   clk, rst;
 
 input  [WIDTH-1:0] idat;
 
 output [WIDTH-1:0] odat;
 reg    [WIDTH-1:0] odat;
 
-always @ (posedge clk or negedge rst_)
+always @ (posedge clk)
     begin
-    if(!rst_) odat <= RESET_VALUE;
+    if(rst) odat <= RESET_VALUE;
     else odat <= idat;
     end
 
