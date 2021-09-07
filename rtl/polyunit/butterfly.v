dda165df6d6c5b6d64c66a3fde53e15e55f9e53d
hakatu <hungnguyen110697@gmail.com>
2021-09-07 09:54:55 +0700

    a

--- rtl/polyunit/butterfly.v
+++ rtl/polyunit/butterfly.v
@@ -2,24 +2,20 @@ module butterfly(
     clk,
     rst,
 
-    a0,//input 24-bit
-    a1,
+    a,//input 24-bit
+    b,
+    w,
 
-    b0,
-    b1,
-
-    c0,//output
+    c0,
     c1,
-    
-    d0,
-    d1,
 
-    sel //mode of operation
+    sel //mode of operation 1: CT 0: GS
 );
 //////////////////
 
 parameter WID = 24;
-parameter SELWID = 9;
+parameter SELWID = 1;
+
 //////////////////
 input clk,
 input rst,
@@ -29,11 +25,5 @@ input [WID-1:0]     a1,
 
 input [WID-1:0]     b0,
 input [WID-1:0]     b1,
-
-    c0,//output
-    c1,
-    
-    d0,
-    d1,
 
 input [SELWID-1:0]     sel //mode of operation
