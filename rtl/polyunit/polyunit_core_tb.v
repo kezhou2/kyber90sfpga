d74c0bd98d7bf78f21cff41c8d24f0b951af93a1
hakatu <hungnguyen110697@gmail.com>
2021-09-19 01:04:30 +0700

    Change project

--- rtl/polyunit/polyunit_core_tb.v
+++ rtl/polyunit/polyunit_core_tb.v
@@ -1,41 +1,41 @@
 module poly_mod_core_tb ();
 
-parameter WIDTH = 12;
-//////////not functional testbench
-reg [WIDTH-1:0] a,b,c;
-reg fail;
-wire [WIDTH:0] ox;
+///////////////////////////////////
 
-initial begin
-  a = 0;
-  b = 0;
-  fail = 0;
-  #100000 if (!fail) $display ("PASS");
-  $stop();
+parameter WIDTH = 12;
+
+//////////not functional testbench
+
+reg [WIDTH-1:0] data_in_tb;
+wire [WIDTH-1:0] data_out_tb;
+
+////////////////////////////////////
+
+always begin
+clk = 0;
+#1 clk =1;
+#1;
 end
 
-polyunit_core #((
+/////////////////////////////
+
+initial begin
+
+end
+
+polyunit_core #(
     .clk(clk),
     .rst(rst),
     
-    .data_in(),
-    .data_out(), //NTT RAM
+    .data_in(data_in_tb),
+    .data_out(data_out_tb), //NTT RAM
 
-    .mode(),//see param
-    .run(),
-    .done()
+    .mode(mode_tb),//see param
+    .run(run_tb),
+    .done(done_tb)
 );
+
 always begin
-  //#50 a = $random;
-  //b = $random;
-  #50 a = 0;
-  b = 4095;
-  c = ((a-b)+3329+3329)%3329; //WARNING CANNOT GET THIS TO WORK CORRECTLY
-  #50 
-  if (ox !== c) begin
-    $display ("Mismatch at time %d",$time);
-    $display (" ket qua la %d",c," nhung lai tinh ra la %d",ox);
-    fail = 1;
-  end
+
 end
 endmodule
